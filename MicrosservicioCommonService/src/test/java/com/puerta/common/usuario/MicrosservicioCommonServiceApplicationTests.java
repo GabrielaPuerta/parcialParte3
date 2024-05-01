e535dd5c8a5a51e249a9208e3364f24429505f8e
GabrielaPuerta <111705848+GabrielaPuerta@users.noreply.github.com>
2024-04-30 23:38:58 -0500

    Add files via upload

---
+++ MicrosservicioCommonService/src/test/java/com/puerta/common/usuario/MicrosservicioCommonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puerta.common.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicioCommonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
